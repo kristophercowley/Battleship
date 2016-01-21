7e1ba27de178b2418fbc1f46966998a1409533b0
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-01-21 15:23:07 -0700

    Dropdown button

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var myArray = [1,2,3,4,5,6,7,8,099];
+var butElem = document.getElementById('butId');
+//Attempting to populate a dropdown button
+for(var i = 0; i < myArray.length; i++){
+	var current = myArray[i];
+	var choice = document.createElement('option');
+	document.appendChild('choice');
+}
